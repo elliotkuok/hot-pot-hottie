e8321169f54e377d90b04064061ef94f86fe9760
Elliot Kuok <elliotckuok@gmail.com>
2023-07-24 09:37:28 -0700

    Edit meter styling

--- src/index.js
+++ src/index.js
@@ -5,21 +5,3 @@ document.addEventListener('DOMContentLoaded', () => {
   
     // Add event listeners for user interactions, e.g., adding food to the hotpot
 });
-
-// Event listener to detect when the player presses a key for picking up the food
-document.addEventListener('keydown', (event) => {
-    const keyPressed = event.key;
-    // Check if the pressed key corresponds to any food type
-    if (keyPressed === 'W' || keyPressed === 'A' || keyPressed === 'S' || keyPressed === 'D') {
-      handlePickUpFood(keyPressed);
-    }
-});
-
-  // Event listener to detect when the player releases a key for dropping the food
-document.addEventListener('keyup', (event) => {
-    const keyReleased = event.key;
-    // Check if the released key corresponds to any food type
-    if (keyPressed === 'W' || keyPressed === 'A' || keyPressed === 'S' || keyPressed === 'D') {
-      handleDropFood(keyReleased);
-    }
-});
